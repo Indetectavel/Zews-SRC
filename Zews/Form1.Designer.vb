23617abad4da442404d39de5f5f1a692b03fc678
Indetectavel <44453659+Indetectavel@users.noreply.github.com>
2019-02-22 21:53:16 -0300

    Add files via upload

---
+++ Zews/Form1.Designer.vb
@@ -0,0 +1,204 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.BunifuDragControl1 = New Bunifu.Framework.UI.BunifuDragControl(Me.components)
+        Me.btnClickMe = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.Panel1.SuspendLayout()
+        CType(Me.btnClickMe, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(32, Byte), Integer), CType(CType(31, Byte), Integer), CType(CType(32, Byte), Integer))
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.PictureBox3)
+        Me.Panel1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Panel1.Location = New System.Drawing.Point(-1, -10)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(328, 58)
+        Me.Panel1.TabIndex = 0
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Label4.Font = New System.Drawing.Font("Verdana", 10.0!, System.Drawing.FontStyle.Bold)
+        Me.Label4.ForeColor = System.Drawing.Color.White
+        Me.Label4.Location = New System.Drawing.Point(298, 26)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(19, 17)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "X"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.Transparent
+        Me.Label3.ForeColor = System.Drawing.Color.White
+        Me.Label3.Location = New System.Drawing.Point(46, 28)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(102, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Zews Tweaker v4.5"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("True Lies", 15.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(74, 78)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(150, 25)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Zews Tweaker"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("True Lies", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(226, 85)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(32, 14)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "4.5"
+        '
+        'Button1
+        '
+        Me.Button1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Button1.Font = New System.Drawing.Font("Verdana", 9.75!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.Color.White
+        Me.Button1.Location = New System.Drawing.Point(126, 330)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 42)
+        Me.Button1.TabIndex = 7
+        Me.Button1.Text = "INSTALL"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'BunifuDragControl1
+        '
+        Me.BunifuDragControl1.Fixed = True
+        Me.BunifuDragControl1.Horizontal = True
+        Me.BunifuDragControl1.TargetControl = Me.Panel1
+        Me.BunifuDragControl1.Vertical = True
+        '
+        'btnClickMe
+        '
+        Me.btnClickMe.Image = Global.Zews.My.Resources.Resources.icons8_mind_map_80
+        Me.btnClickMe.Location = New System.Drawing.Point(113, 170)
+        Me.btnClickMe.Name = "btnClickMe"
+        Me.btnClickMe.Size = New System.Drawing.Size(101, 85)
+        Me.btnClickMe.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.btnClickMe.TabIndex = 6
+        Me.btnClickMe.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.PictureBox2.Image = Global.Zews.My.Resources.Resources.icons8_Play_Button_48px_1
+        Me.PictureBox2.Location = New System.Drawing.Point(27, 375)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(49, 44)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox2.TabIndex = 4
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.PictureBox1.Image = Global.Zews.My.Resources.Resources.twitter_icone
+        Me.PictureBox1.Location = New System.Drawing.Point(244, 375)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(49, 44)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 3
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Image = Global.Zews.My.Resources.Resources.icons8_mind_map_80
+        Me.PictureBox3.Location = New System.Drawing.Point(12, 21)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(32, 26)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox3.TabIndex = 5
+        Me.PictureBox3.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(26, Byte), Integer), CType(CType(26, Byte), Integer), CType(CType(26, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(321, 431)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.btnClickMe)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Cursor = System.Windows.Forms.Cursors.Default
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.btnClickMe, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents btnClickMe As PictureBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents BunifuDragControl1 As Bunifu.Framework.UI.BunifuDragControl
+End Class
---
+++ Zews/Form1.vb
@@ -0,0 +1,103 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim num As Integer = 0
+        Do
+            num += 1
+        Loop While num <= 100
+        Me.netsh()
+        Me.reg()
+        Interaction.MsgBox("Tweaker V4.5 Intalada com sucesso!", MsgBoxStyle.Information, "Zews Tweaker V4.5")
+    End Sub
+
+    Public Sub netsh()
+        Me.cmdnetsh("netsh int tcp set global timestamps=disabled")
+        Me.cmdnetsh("netsh int tcp set global chimney=enabled")
+        Me.cmdnetsh("netsh int tcp set global netdma=enabled")
+        Me.cmdnetsh("netsh int tcp set global autotuninglevel=normal")
+        Me.cmdnetsh("netsh int tcp set global ecncapability=disabled")
+        Me.cmdnetsh("netsh int tcp set global congestionprovider=cubic")
+        Me.cmdnetsh("netsh int tcp set global dca=enabled")
+        Me.cmdnetsh("netsh int ipv4 set dynamicport tcp start=1025 num=64511")
+        Me.cmdnetsh("netsh int ipv4 set dynamicport protocol=udp start1025 num=64511")
+        Me.cmdnetsh("netsh int ipv6 set dynamicport tcp start=1025 num=64511")
+        Me.cmdnetsh("netsh int ipv6 set dynamicport protocol=udp start1025 num=64511")
+        Me.cmdnetsh("netsh int tcp set heuristics disabled")
+    End Sub
+
+    Public Sub reg()
+        Me.cmd("HKCU\SOFTWARE\Microsoft\Windows\Current\Version\Explorer\Advanced /v LastActiveClick /t REG_DWORD /d 1 /f")
+        Me.cmd("HKCU\SOFTWARE\Microsoft\Windows\Current\Version\Explorer\Advanced /v DisallowShaking /t REG_DWORD /d 1 /f")
+        Me.cmd("HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile /v SystemResponsiveness /t REG_DWORD /d 0 /f")
+        Me.cmd("HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile /v LargeSystemCache /t REG_DWORD /d 0 /f")
+        Me.cmd("HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile\Tasks\Games /v Affinity /t REG_DWORD /d 0 /f")
+        Me.cmd("HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile\Tasks\Games /v Background Only /t REG_SZ /d False /f")
+        Me.cmd("HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile\Tasks\Games /v Clock Rate /t REG_DWORD /d 10000 /f")
+        Me.cmd("HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile\Tasks\Games /v GPU Priority /t REG_DWORD /d 8 /f")
+        Me.cmd("HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile\Tasks\Games /v Priority /t REG_DWORD /d 6 /f")
+        Me.cmd("HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile\Tasks\Games /v SFIO Priority /t REG_SZ /d High /f")
+        Me.cmd("HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile /v NetworkThrottlingIndex /t REG_DWORD /d 4294967295 /f")
+        Me.cmd("HKLM\SOFTWARE\Microsoft\MSMQ\Parameters /v TCPNoDelay /t REG_DWORD /d 1 /f")
+        Me.cmd("HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v EnableBalloonTips /t REG_DWORD /d 0 /f")
+        Me.cmd("HKLM\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters /v IRPStackSize /t REG_DWORD /d 50 /f")
+        Me.cmd("HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer /v NoLowDiskSpaceChecks /t REG_DWORD /d 1 /f")
+        Me.cmd("HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer /v LinkResolveIgnoreLinkInfo /t REG_DWORD /d 1 /f")
+        Me.cmd("HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer /v NoResolveSearch /t REG_DWORD /d 1 /f")
+        Me.cmd("HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer /v NoResolveTrack /t REG_DWORD /d 1 /f")
+        Me.cmd("HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer /v NoInternetOpenWith /t REG_DWORD /d 1 /f")
+        Me.cmd("HKLM\SYSTEM\ControlSet002\Control /v WaitToKillServiceTimeout /t REG_SZ /d 1000 /f")
+        Me.cmd("HKCU\Control Panel\Desktop /v AutoEndTasks /t REG_SZ /d 1 /f")
+        Me.cmd("HKCU\Control Panel\Desktop /v WaitToKillAppTimeout /t REG_SZ /d 2000 /f")
+        Me.cmd("HKU\S-1-5-20\Control Panel\Desktop /v AutoEndTasks /t REG_SZ /d 1 /f")
+        Me.cmd("HKU\S-1-5-20\Control Panel\Desktop /v HungAppTimeout /t REG_SZ /d 1000 /f")
+        Me.cmd("HKU\S-1-5-20\Control Panel\Desktop /v MenuShowDelay /t REG_SZ /d 0 /f")
+        Me.cmd("HKU\S-1-5-20\Control Panel\Desktop /v WaitToKillAppTimeout /t REG_SZ /d 2000 /f")
+        Me.cmd("HKLM\SYSTEM\CurrentControlSet\Control /v WaitToKillServiceTimeout /t REG_SZ /d 2000 /f")
+        Me.cmd("HKCU\Control Panel\Desktop /v HungAppTimeout /t REG_SZ /d 1000 /f")
+        Me.cmd("HKCU\Control Panel\Desktop /v MenuShowDelay /t REG_SZ /d 8 /f")
+        Me.cmd("HKCU\Control Panel\Desktop /v LowLevelHooksTimeout /t REG_SZ /d 1000 /f")
+        Me.cmd("HKLM\SYSTEM\CurrentControlSet\Services\Pshed /v NonBestEffortLimit /t REG_DWORD /d 0 /f")
+        Me.cmd("HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters /v EnableDCA /t REG_DWORD /d 1 /f")
+        Me.cmd("HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters /v EnableWsd /t REG_DWORD /d 0 /f")
+        Me.cmd("HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters /v EnableTCPA /t REG_DWORD /d 1 /f")
+        Me.cmd("HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters /v DisableTaskOffload /t REG_DWORD /d 0 /f")
+        Me.cmd("HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters /v EnableConnectionRateLimiting /t REG_DWORD /d 0 /f")
+        Me.cmd("HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters /v TcPMaxDataRetransmissions /t REG_DWORD /d 5 /f")
+        Me.cmd("HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters /v TcpNumConnections /t REG_DWORD /d 1280 /f")
+        Me.cmd("HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters /v TcpTimedWaitDelay /t REG_DWORD /d 25 /f")
+        Me.cmd("HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters /v SynAttackProtect /t REG_DWORD /d 1 /f")
+        Me.cmd("HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters /v TcpCreateAndConnectTcbRateLimitDepth /t REG_DWORD /d 0 /f")
+        Me.cmd("HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters /v TCPNoDelay /t REG_DWORD /d 1 /f")
+        Me.cmd("HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters /v TcpAckFrequency /t REG_DWORD /d 1 /f")
+        Me.cmd("HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters /v TcpMaxDataRetransmissions /t REG_DWORD /d 5 /f")
+        Me.cmd("HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters /v TCPDelAckTicks /t REG_DWORD /d 0 /f")
+        Me.cmd("HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters /v Tcp1323Opts /t REG_DWORD /d 3 /f")
+        Me.cmd("HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters /v MaxFreeTcbs /t REG_DWORD /d 415030 /f")
+        Me.cmd("HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters /v MaxUserPort /t REG_DWORD /d 415028 /f")
+        Me.cmd("HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters /v DefaultTTL /t REG_DWORD /d 100 /f")
+        Me.cmd("HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters /v GlobalMaxTcpWindowSize /t REG_DWORD /d 415029 /f")
+        Me.cmd("HKLM\SOFTWARE\Policies\Microsoft\Windows /v NonBestEffortLimit /t REG_DWORD /d 0 /f")
+        Me.cmd("HKCU\Control Panel\Mouse /v MouseSensitivity /t REG_SZ /d 10 /f")
+    End Sub
+
+    Public Sub cmd(arguments As String)
+        Dim startInfo As ProcessStartInfo = New ProcessStartInfo() With {.FileName = "reg.exe", .Arguments = "ADD " + arguments, .UseShellExecute = True, .WindowStyle = ProcessWindowStyle.Hidden}
+        Process.Start(startInfo)
+    End Sub
+
+    Public Sub cmdnetsh(arguments As String)
+        Dim startInfo As ProcessStartInfo = New ProcessStartInfo() With {.FileName = "cmd.exe", .Arguments = arguments, .UseShellExecute = True, .WindowStyle = ProcessWindowStyle.Hidden}
+        Process.Start(startInfo)
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        Process.Start("https://twitter.com/Indetectavell")
+    End Sub
+
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+        Process.Start("https://www.youtube.com/channel/UCOSE1SSCsvbJ8kg07TwlcVA?view_as=subscriber")
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
+        Me.Close()
+    End Sub
+End Class
